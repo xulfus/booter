7f599d1467a4d3cfec8fcde811616305b0654827
Janne Haarni <janne.haarni@gofore.com>
2016-01-18 14:28:05 +0200

    fixed database url

--- src/main/java/com/gofore/booter/BooterApplication.java
+++ src/main/java/com/gofore/booter/BooterApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BooterApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BooterApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BooterApplication.class, args);
+    }
 
 
 }
