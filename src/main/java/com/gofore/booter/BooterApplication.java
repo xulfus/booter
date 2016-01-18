c30cc2989f148becf2b9f14e50b19a5302b37199
Janne Haarni <janne.haarni@gofore.com>
2016-01-18 13:23:41 +0200

    initial import

---
+++ src/main/java/com/gofore/booter/BooterApplication.java
@@ -0,0 +1,14 @@
+package com.gofore.booter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooterApplication.class, args);
+	}
+
+
+}
